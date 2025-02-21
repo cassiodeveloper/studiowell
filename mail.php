9a35f252d30974039e43ebc68891ac2f3995a602
Cássio B. Pereira <cassio@cassiobp.com.br>
2025-02-21 10:38:38 +0100

    V0

---
+++ mail.php
@@ -0,0 +1,63 @@
+<?php
+/*
+Template Name: HaarMax
+Template URL: http://vecurosoft.com/products/html/haarmax
+Description: HaarMax - Barber and Salon HTML Template
+Author: Vecuro
+Author URI: https://www.templatemonster.com/authors/vecuro/
+Version: 1.0.0
+*/
+
+    // Only process POST reqeusts.
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Get the form fields and remove whitespace.
+        $name = strip_tags(trim($_POST["name"]));
+        $name = str_replace(array("\r","\n"),array(" "," "),$name);
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $number = trim($_POST["number"]);
+        $subject = trim($_POST["subject"]);
+        $message = trim($_POST["message"]);
+
+        // Check that data was sent to the mailer.
+        if ( empty($name) OR empty($message) OR empty($number) OR empty($subject) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // Set a 400 (bad request) response code and exit.
+            http_response_code(400);
+            echo "Please complete the form and try again.";
+            exit;
+        }
+
+        // Set the recipient email address.
+        // FIXME: Update this to your desired email address.
+        $recipient = "vecuroproducts@gmail.com";
+
+        // Set the email subject.
+        $subject = "New contact from $subject";
+
+        // Build the email content.
+        $email_content = "Name: $name\n";
+        $email_content .= "Subject: $subject\n";
+        $email_content .= "Number: $number\n";
+        $email_content .= "Email: $email\n\n";
+        $email_content .= "Message:\n$message\n";
+
+        // Build the email headers.
+        $email_headers = "From: $name <$email>";
+
+        // Send the email.
+        if (mail($recipient, $subject, $email_content, $email_headers)) {
+            // Set a 200 (okay) response code.
+            http_response_code(200);
+            echo "Thank You! Your message has been sent.";
+        } else {
+            // Set a 500 (internal server error) response code.
+            http_response_code(500);
+            echo "Oops! Something went wrong and we couldn't send your message.";
+        }
+
+    } else {
+        // Not a POST request, set a 403 (forbidden) response code.
+        http_response_code(403);
+        echo "There was a problem with your submission, please try again.";
+    }
+
+?>
